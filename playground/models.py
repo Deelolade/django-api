21d0981638facd2b4a8940345d3e276c0646caad
Deelolade <oluwanisholahabeeb13@icloud.com>
2026-04-13 11:27:32 -0700

    updated the blog models

--- playground/models.py
+++ playground/models.py
@@ -5,3 +5,9 @@ from django.db import models
 class Posts (models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    slug = models.SlugField(unique=True)
+    author = models.CharField(max_length=100)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)
+
--- playground/urls.py
+++ playground/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 # URL config
 urlpatterns = [
-    path('hello/', views.say_hello),
-    path('user/', views.get_user_data)
+    # path('hello/', views.say_hello),
+    # path('user/', views.get_user_data),
+    path('', views.home),
+    path('post/<int:id>/', views.post)
 ]
